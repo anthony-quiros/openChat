958fab6715b353e463f086725aa74b1f03b199ae
Lucas Paolotti <lpaolotti@gmail.com>
2015-01-22 16:11:27 +0100

    popin insertion d'images
    Ajout d'une popin permettant d'insérer une image via une URL en
    spécifiant les dimensions si besoin est.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://aquiros.fr.cr');
+var socket = io.connect('http://localhost:8080');
 var getMessage = function(message) {
 	var list = document.getElementById("listOfMessages");
 	list.innerHTML = list.innerHTML + "<br/>" + message;
@@ -18,3 +18,23 @@ var sendAlias = function (message) {
 socket.on('message',getMessage);
 /*socket.on("alias", sendAlias);*/
 document.getElementById("sendMessage").addEventListener("click", sendMessage);
+
+$( document ).ready(function() {
+    $('.sendImage').fancybox({
+		minWidth	: 520,
+		minHeight	: 25,
+		width	: 520,
+		height	: 25,
+		fitToView	: false,
+		autoSize	: false,
+		closeClick	: false,
+		openEffect	: 'none',
+		closeEffect	: 'none'
+	});
+	
+	$('#sendImg').click(function(){
+	var imgWidth = parseInt($("#imgWidth").val()) > 0 ? $("#imgWidth").val() : "200px";
+	var imgHeight = parseInt($("#imgHeight").val()) > 0 ? $("#imgHeight").val() : "300px";
+		socket.emit("message", "<img src=" + $("#valueImg").val() + " width="+ imgWidth +" height=" + imgHeight +"/>");
+	});
+});
