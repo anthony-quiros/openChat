36ec3e5eeb545c3b72c0ce94b9cb7fe71766facf
Lucas Paolotti <lpaolotti@gmail.com>
2015-01-28 14:38:32 +0100

    Création d'un JS d'utilitaires
    Scission du JS client en plusieurs JS pour éviter d'alourdir le fichier
    principal.

--- public/js/chat.js
+++ public/js/chat.js
@@ -113,23 +113,6 @@ function generateTable(){
 	html += "</table>";
 	return html;
 }
-
-/** Determine si la chaine passée en argument est encodée (urlEncode)**/
-function isEncHTML(str) {
-  if(str.search(/&amp;/g) != -1 || str.search(/&lt;/g) != -1 || str.search(/&gt;/g) != -1 || str.search(/&quot;/g) != -1){
-	 return true;
-  }
-  return false;
-};
- 
-/** Permet de décoder la chaine encodée (urlEncore)**/
-function decHTMLifEnc(str){
-    if(isEncHTML(str)){
-		return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
-	} 
-    return str;
-}
-
 /** Initialisation des popins du chat **/
 function initChatPopins(){
 	// Popin d'insertion d'image
---
+++ public/js/tools.js
@@ -0,0 +1,15 @@
+/** Determine si la chaine passée en argument est encodée (urlEncode)**/
+function isEncHTML(str) {
+  if(str.search(/&amp;/g) != -1 || str.search(/&lt;/g) != -1 || str.search(/&gt;/g) != -1 || str.search(/&quot;/g) != -1){
+	 return true;
+  }
+  return false;
+};
+ 
+/** Permet de décoder la chaine encodée (urlEncore)**/
+function decHTMLifEnc(str){
+    if(isEncHTML(str)){
+		return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
+	} 
+    return str;
+}
