e2b174fab6e94d882f0174f94b899225fc37c8a5
Lucas Paolotti <lpaolotti@gmail.com>
2015-01-28 11:42:52 +0100

    Problème d'affichage des message contenant des balises html coté recepteur Fixes #8
    Si le message contenait des balises html coté recepteur, celle-ci
    étaient parsées en texte à tort (message urlEncoded non decodé).
    Une chaîne contenant un émoticone était alors parsé en texte au lieu
    d'afficher l'image.

--- public/js/chat.js
+++ public/js/chat.js
@@ -8,8 +8,8 @@ var emoticons = [
 ]
 
 var getMessage = function(message) {
-	var list = document.getElementById("listOfMessages");
-	list.innerHTML = list.innerHTML + "<br/>" + message;
+	var messageToAppend = isEncHTML(message) ? decHTMLifEnc(message) : message;
+	$("#listOfMessages").html("<br/>" + messageToAppend);
 };
 
 function sendImage() {
@@ -112,6 +112,22 @@ function generateTable(){
 	}
 	html += "</table>";
 	return html;
+}
+
+/** Determine si la chaine passée en argument est encodée (urlEncode)**/
+function isEncHTML(str) {
+  if(str.search(/&amp;/g) != -1 || str.search(/&lt;/g) != -1 || str.search(/&gt;/g) != -1 || str.search(/&quot;/g) != -1){
+	 return true;
+  }
+  return false;
+};
+ 
+/** Permet de décoder la chaine encodée (urlEncore)**/
+function decHTMLifEnc(str){
+    if(isEncHTML(str)){
+		return str.replace(/&amp;/g, '&').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&quot;/g, '"');
+	} 
+    return str;
 }
 
 /** Initialisation des popins du chat **/
