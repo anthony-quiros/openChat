e0aab7ca7cd2376a7bcb943bd0adf61d13b34748
ant D soft <quiros.antho@gmail.com>
2015-03-19 10:47:10 +0100

    Correction

--- public/js/mobileChat.js
+++ public/js/mobileChat.js
@@ -3,9 +3,11 @@ $('#sendAlias').click(sendAlias);
 getImage = function(message) {}
 showAliasForm = function() {
 	document.querySelector("#aliasPopin").setAttribute("class", "");
+	document.querySelector("#write").setAttribute("class", "hidden");
 }
 closeAliasForm = function() {
 	document.querySelector("#aliasPopin").setAttribute("class", "hidden");
+	document.querySelector("#write").setAttribute("class", "");
 }
 getFile = function(message) {}
 createMemberElement  = function(message) {}
